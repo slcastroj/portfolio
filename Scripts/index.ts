cf1a96588b097dd70af8415b2f6b8e4a474bebdd
slcastroj <slcastroj@gmail.com>
2019-09-16 12:33:29 -0300

    Added base modals

--- Scripts/index.ts
+++ Scripts/index.ts
@@ -1,6 +1,5 @@
 $(() => {
-    const mwLockTime = 500;
-    const mwAnimationTime = 1000;
+    const mwLockTime = 700;
     let mwLock = false
     let vh = $(window).height()
 
@@ -18,6 +17,34 @@ $(() => {
         setTimeout(() => { mwLock = false }, mwLockTime)
         let st = $(document).scrollTop()
         st += -Math.sign(e.deltaY) * vh
+        $(document).scrollTop(st)
+    })
+
+    $(document).keydown((e) => {
+        e.preventDefault()
+        return false
+    })
+
+    $(document).keyup((e) => {
+        if (mwLock) {
+            e.preventDefault()
+            return false
+        }
+        mwLock = true
+        setTimeout(() => { mwLock = false }, mwLockTime)
+        let delta = 0
+        let key = e.which
+        if (key == 38 || key == 37) {
+            delta = -1
+        } else if (key == 39 || key == 40) {
+            delta = 1
+        }
+        else {
+            e.preventDefault()
+            return false
+        }
+        let st = $(document).scrollTop()
+        st += delta * vh
         $(document).scrollTop(st)
     })
 })
